70052a6e791364bf5fe3396b4b672cb46bc9c182
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-08 19:49:57 -0700

    hbnb

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class"""
+    def __init__(self):
+        """initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """str"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        """save obj"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """returns dict"""
+        dic = self.__dict__.copy()
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
