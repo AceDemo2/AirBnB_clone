d501b5c3ed51bd286fbfd86f1d403dc1264ae800
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-08 20:37:56 -0700

    hbnb

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,19 @@ from datetime import datetime
 
 class BaseModel:
     """Base class"""
-    def __init__(self):
+    def __init__(self, *arg, **kwargs):
         """initialization"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.fromisoformat(value)
+                elif k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
         """str"""
