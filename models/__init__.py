07241eecfa49952ef22e639737fcc55b0247ab69
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-12 10:17:00 -0700

    hbnb

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-from models.engine import file_storage
-storage = file_storage.FileStorage()
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+storage FileStorage()
 storage.reload()
