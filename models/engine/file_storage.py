1fafb71bcf1303e02300689c321edd9dda9ee250
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-12 20:38:31 -0700

    hbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
 import os
-
+from model.user import User
 
 class FileStorage:
     """store in json"""
@@ -23,21 +23,14 @@ class FileStorage:
             for k, v in self.__objects.items():
                 obj[k] = v.to_dict()
             json.dump(obj, f)
-
-    """def reload(self):
-       ""de-serialize persisted objects""
-        try:
-            deserialized = {}
-            with open(self.__file_path, "r") as f:
-                deserialized = json.loads(f.read())
-            self.__objects = {
-                key:
-                    eval(obj["__class__"])(**obj)
-                    for key, obj in deserialized.items()}
-        except (FileNotFoundError, JSONDecodeError):
-            # No need for error
-            pass"""
-
+    
+    def classes(self):
+        """return classes"""
+        from models.base_model import BaseModel
+        classes = {"BaseModel": BaseModel,
+                    "User": User}
+        return classes
+    
     def reload(self):
         """read from json"""
         if os.path.exists(self.__file_path):
@@ -45,6 +38,6 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 obj = json.load(f)
             for k, v in obj.items():
-                #clsn = v['__class__']
+                clsn = v['__class__']
                 # cls = global().get(clsn)
-                self.__objects[k] = eval(v['__class__'])(**v)
+                self.__objects[k] = self.classes()[v](**v)
